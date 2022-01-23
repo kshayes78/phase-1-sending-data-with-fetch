c4f3182945115c50e6b9437cd47b92b296fd1a24
kshayes78 <89166628+kshayes78@users.noreply.github.com>
2022-01-23 18:40:07 -0500

    commit tree ea3fc3c9791246a00a7762f2edc98678bb19dee2

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Add your code here
+/*fetch("http://localhost:3000/dogs", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+  body: JSON.stringify({
+    dogName: "Byron",
+    dogBreed: "Poodle",
+  }),
+});*/
+
+function submitData(name, email) {
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      name,
+      email,
+    }),
+  })
+    .then((response) => response.json())
+    .then((newObj) => (document.body.innerHTML = newObj["id"]))
+    .catch(function (error) {
+      alert("Uh-oh");
+      document.body.innerHTML = error.message;
+    });
+}
